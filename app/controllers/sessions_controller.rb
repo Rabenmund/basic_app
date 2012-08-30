b13b6eb164464f0ea0e8c8fa9cb05466db9d0f96
Wiedenbruch-Invision <detlef.wiedenbruch@invision.de>
2012-08-30 09:53:18 +0200

    authenticate with email or nickname

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:session][:email])
+    user = User.find_by_email(params[:session][:auth]) || User.find_by_nickname(params[:session][:auth])
     if user && user.authenticate(params[:session][:password])
       sign_in user
       redirect_back_or home_path
