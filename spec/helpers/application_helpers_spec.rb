414211b125c4c3d30e380f09bfe2d664bca05cca
Wiedenbruch-Invision <detlef.wiedenbruch@invision.de>
2012-08-31 10:56:14 +0200

    simplified helper tests

--- spec/helpers/application_helpers_spec.rb
+++ spec/helpers/application_helpers_spec.rb
@@ -2,17 +2,15 @@ require 'spec_helper'
 
 describe ApplicationHelper do
 
-  describe "full_title" do
-    it "should include the page name" do
-      full_title("foo").should =~ /foo/
-    end
-
-    it "should include the base name" do
-      full_title("foo").should =~ /^Ruby on Rails Tutorial Sample App/
-    end
-
-    it "should not include a bar for the home page" do
-      full_title("").should_not =~ /\|/
-    end
+  describe :full_title do
+    subject { full_title "foo" }
+    it { should =~ /foo/ }
+    it { should =~ /^Mein.Programm/ }
   end
+  
+  describe :distance_of_time_in_words do
+    subject { distance_of_time_in_words(1.hour.ago, Time.now) }
+    it { should eq "etwa eine Stunde" }
+  end
+  
 end
